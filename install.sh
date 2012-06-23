0d16e88f9a618aac291702ce837f4b259f36f0da
Marwan Osman <lordm2005@gmail.com>
2012-06-23 23:49:38 +0200

    Added patched font ubuntu-mono for powerline, updated install script to work on PWD

--- install.sh
+++ install.sh
@@ -5,21 +5,23 @@ git submodule --quiet update --init
 
 echo "Deleting old files..."
 rm ~/.vimrc
-rm ~/.vim
+rm -rf ~/.vim
 #rm ~/.gvimrc
 #rm ~/.gitconfig
 #rm ~/.gitignore
 #rm ~/.zshrc
 #rm ~/.tmux.conf
+rm -rf ~/.fonts/ubuntu-mono
 
 echo "Symlinking files..."
-ln -fs ~/workspace/dotfiles/vim/vimrc ~/.vimrc
-ln -fs ~/workspace/dotfiles/vim ~/.vim
-#ln -fs ~/workspace/dotfiles/vim/gvimrc ~/.gvimrc
-#ln -fs ~/workspace/dotfiles/zshrc ~/.zshrc
-#ln -fs ~/workspace/dotfiles/gitconfig ~/.gitconfig
-#ln -fs ~/workspace/dotfiles/gitignore ~/.gitignore
-#ln -fs ~/workspace/dotfiles/tmux.conf ~/.tmux.conf
+ln -fs "$PWD/vim/vimrc" ~/.vimrc
+ln -fs "$PWD/vim" ~/.vim
+ln -fs "$PWD/fonts/ubuntu-mono" ~/.fonts/ubuntu-mono
+#ln -fs "$PWD/vim/gvimrc" ~/.gvimrc
+#ln -fs "$PWD/zshrc" ~/.zshrc
+#ln -fs "$PWD/gitconfig" ~/.gitconfig
+#ln -fs "$PWD/gitignore" ~/.gitignore
+#ln -fs "$PWD/tmux.conf" ~/.tmux.conf
 
 echo "Updating submodules..."
 git submodule foreach git pull origin master
