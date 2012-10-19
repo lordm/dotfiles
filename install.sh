2122fec5cd27ee3b3e0434f079c7979316cb39cb
Marwan Osman <lordm2005@gmail.com>
2012-10-19 23:22:48 +0200

    Added gvimrc file

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git submodule --quiet update --init
 echo "Deleting old files..."
 rm ~/.vimrc
 rm -rf ~/.vim
-#rm ~/.gvimrc
+rm ~/.gvimrc
 rm ~/.gitconfig
 #rm ~/.gitignore
 #rm ~/.zshrc
@@ -16,12 +16,12 @@ rm -rf ~/.fonts/ubuntu-mono
 echo "Symlinking files..."
 ln -fs "$PWD/vim/vimrc" ~/.vimrc
 ln -fs "$PWD/vim" ~/.vim
-ln -fs "$PWD/fonts/ubuntu-mono" ~/.fonts/ubuntu-mono
-#ln -fs "$PWD/vim/gvimrc" ~/.gvimrc
-#ln -fs "$PWD/zshrc" ~/.zshrc
+ln -fs "$PWD/vim/gvimrc" ~/.gvimrc
 ln -fs "$PWD/gitconfig" ~/.gitconfig
 #ln -fs "$PWD/gitignore" ~/.gitignore
+#ln -fs "$PWD/zshrc" ~/.zshrc
 #ln -fs "$PWD/tmux.conf" ~/.tmux.conf
+ln -fs "$PWD/fonts/ubuntu-mono" ~/.fonts/ubuntu-mono
 
 echo "Updating submodules..."
 git submodule foreach git pull origin master
