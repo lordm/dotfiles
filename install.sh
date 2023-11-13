1dbdad90adc2973843f410f12b4a3bd2e9b01168
Marwan Osman <marwan.aosman@gmail.com>
2023-11-13 13:42:08 +0200

    Add lazygit and floaterm

--- install.sh
+++ install.sh
@@ -10,6 +10,13 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 
+# install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+
 echo "Backing up the old files..."
 cp ~/.vimrc ~/vimrc.old
 cp ~/.gvimrc ~/.gvimrc.old
