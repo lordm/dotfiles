379c68919e3367ce9001c14e1e516d828d4500fc
Marwan Osman <marwan.aosman@gmail.com>
2023-11-13 13:38:59 +0200

    misc. updates

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+
 echo "Backing up the old files..."
 cp ~/.vimrc ~/vimrc.old
 cp ~/.gvimrc ~/.gvimrc.old
